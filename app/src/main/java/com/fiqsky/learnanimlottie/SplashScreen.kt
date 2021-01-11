8fac3e487c23563ac28381a9656c2f2949b5ae49
FiqSky <fiqihfm05@gmail.com>
2021-01-11 14:18:13 +0700

    Initial commit

---
+++ app/src/main/java/com/fiqsky/learnanimlottie/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.fiqsky.learnanimlottie
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+
+    private var timeOut: Long = 6000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, timeOut)
+    }
+}
